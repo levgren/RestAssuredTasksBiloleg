3a300ba3e06ed96b3da84b37ce6f2d904a3d45f3
levgren <jekaobodianskyi@gmail.com>
2019-09-03 15:00:15 +0300

    +

--- src/test/java/getRequestsTasks/GetListResources.java
+++ src/test/java/getRequestsTasks/GetListResources.java
@@ -1,12 +1,29 @@
 package getRequestsTasks;
 
+import io.restassured.http.ContentType;
 import org.junit.Test;
 
-public class GetListResources {
-    //LIST <RESOURCE>
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.hasItem;
 
+public class GetListResources {
+
+    //LIST <RESOURCE>
     @Test
     public void listResources(){
-        
+        given()
+                .baseUri("https://reqres.in/")
+                .contentType(ContentType.JSON)
+                .basePath("/api/unknown")
+                .param("page", 2)
+                .when()
+                .get()
+                .then()
+                .statusCode(200)
+                .and()
+                .body("name", hasItem("cerulean"));
+//                .and()
+//                .body("year", hasItem(2000));
+
     }
 }
